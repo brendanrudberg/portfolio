4dbe165f4471471af6036e56da798537a919c986
brendanrudberg <rudbergbrendan@gmail.com>
2026-07-16 16:19:46 +0000

    Added Daedalus 3 Project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,7 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-import remarkMath from "remark-math";
-import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  markdown: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-  },
 });
